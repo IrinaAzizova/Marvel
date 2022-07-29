18692a41a202605125251b680296a69230ee45bd
Azizova Irina <i_r_i_n_a_nikitina@mail.ru>
2022-07-29 18:15:35 +0300

    implemented showing a random character on page load

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,12 +12,33 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?apikey=bce4864d4864972f92a041912036d003`);
+        return this._transformCharacter(res.data.results[0])
+    }
+
+    _transformCharacter = (char) => {
+        let descr;
+        if (!char.descr) {
+            descr = 'Character description missing';
+        } else if (char.descr.length > 150) {
+            descr = char.descr.slice(0, 149) + '...';
+        } else {
+            descr = char.descr;
+        }
+        
+        return {
+            name: char.name,
+            description: descr,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
